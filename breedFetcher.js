36dd5a40d7e6b18e6bcc6962d6a00c88eb7253b4
masinette <samuelstoniann@gmail.com>
2021-02-11 15:39:06 -0500

    FIXED URL

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,7 +5,49 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-// console.log(request);
+
+//Allow the user to specify the breed name using command-line arguments.
+//const catName = process.argv[2];  //to get user input for breed <-------- USE THIS VERSION
+
+rl.question("What is the breed?", userBreedInput => {
+  console.log(`Breed: ${userBreedInput}`);
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${userBreedInput}`, (error, response, body) => {
+    
+    // console.log("ERROR", error);
+    
+    if (error) {
+      console.log("This page doesn't exist");
+      rl.close();
+      return;
+    } else {
+      
+      
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      
+      let parsedBody = JSON.parse(body);
+      //if the parsedBody returns an empty array, display error message
+      if (parsedBody.length === 0) {
+        console.log('error:', 'This breed is not in our database'); // Print the error if one occurred
+      } else {
+        console.log(parsedBody[0].description); // Print the description for the breed
+      }
+      
+      
+      
+      rl.close();
+    }
+  });
+  
+  
+});
+
+
+
+
+
+
+
 
 /*
 Write the logic in breedFetcher.js to fetch the
@@ -25,45 +67,3 @@ Siberian data from the API endpoint using request.
 //   //date is being returned as a string, so deserialize it (parse to object)
 //   // JSON.parse(body);
 // });
-//Allow the user to specify the breed name using command-line arguments.
-//const catName = process.argv[2];  //to get user input for breed <-------- USE THIS VERSION
-
-rl.question("What is the breed?", userBreedInput => {
-  console.log(`Breed: ${userBreedInput}`);
-
-  request(`https:/api.thecatapi.com/v1/breeds/search?q=${userBreedInput}`, (error, response, body) => {
-
-    // console.log("ERROR", error);
-
-    if (error) {
-      console.log("This page doesn't exist");
-      rl.close();
-      return;
-    } else {
-
-
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-      let parsedBody = JSON.parse(body);
-      //if the parsedBody returns an empty array, display error message
-      if (parsedBody.length === 0) {
-        console.log('error:', 'This breed is not in our database'); // Print the error if one occurred
-      } else {
-        console.log(parsedBody[0].description); // Print the description for the breed
-      }
-
-
-
-      rl.close();
-    }
-  });
-
-
-});
-
-
-
-
-
-
-
